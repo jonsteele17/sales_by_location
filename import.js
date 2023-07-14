fc81e0d6c3fa5e773b12aaa4eacff63f41ec21d9
Jonathan Steele <jonsteele94@gmail.com>
2023-07-14 11:05:48 -0500

    renamed xls

--- import.js
+++ import.js
@@ -1,10 +1,7 @@
 import { createPool } from "mysql"; // library (package) npmjs.org
 import XLSX from "xlsx"; // library (package) npmjs.org
 
-const filename = Directory.getFiles(
-  "C:UsersJonathanSteelegit",
-  "*YTD Sales Report*.xls"
-).FirstOrDefault;
+const filename = "YTD Sales Report - JonS.xls";
 const expectedSheetName = "Sheet1"; // The name of the sheet in the excel file
 const headerRow = 4; // The row number of the header row in the excel file
 
